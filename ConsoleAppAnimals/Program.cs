a92194d639feb4dede26b1c1fdb49bf02c8124eb
jules <jules.wagheamcker99@gmail.com>
2023-03-07 16:13:47 +0100

    3

--- ConsoleAppAnimals/Program.cs
+++ ConsoleAppAnimals/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleAppAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal("Lion");
+
+            myAnimal.Move(); // Affiche "Move"
+            myAnimal.Sleep(); // Affiche "Sleep"
+
+            Console.ReadLine();
+        }
+    }
+}
